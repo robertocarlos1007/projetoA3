960fa48d5d2f8776c824941a2621992e887b5ae4
robertocarlos1007 <128102241+robertocarlos1007@users.noreply.github.com>
2023-06-16 20:04:36 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,91 @@
+<?php
+include("conexao.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+    $login = isset($_POST['login']) ? $_POST['login'] : '';
+    $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
+
+    // Verificar se o nome de login já está em uso
+    $verificarLogin = "SELECT * FROM login WHERE login = '$login'";
+    $resultado = mysqli_query($conexao, $verificarLogin);
+
+    if (mysqli_num_rows($resultado) > 0) {
+        echo "O nome de login '$login' já está em uso. Por favor, escolha outro.";
+    } else {
+        $insert = "INSERT INTO login (nome, login, senha) VALUES ('$nome', '$login', '$senha')";
+        if (mysqli_query($conexao, $insert)) {
+            header('Location: index.php');
+            exit();
+        } else {
+            echo "Erro ao cadastrar usuário: " . mysqli_error($conexao);
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(45deg,orange,cyan);
+        }
+
+        div {
+            background-color: rgba(0, 0, 0,0.9);
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            padding: 80px;
+            border-radius: 15px;
+            color:;
+        }
+
+        input {
+            padding: 15px;
+            border: none;
+            outline: none;
+            font-size: 15px;
+        }
+
+        #entrar {
+            display: block;
+            margin: 0 auto;
+            width: 150px;
+            height: 40px;
+            cursor: pointer; /* Adicionando efeito de mãozinha ao cursor */
+            animation: neon-border 2s infinite;
+        }
+
+        @keyframes neon-border {
+            0% {
+                border-color: #ff00ff;
+            }
+            50% {
+                border-color: #ff00ff;
+            }
+            100% {
+                border-color: #ff00ff;
+            }
+        }
+    </style>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <center>
+        <h1>Cadastro</h1>
+        <form id="cadastro" action="cadastro.php" method="POST">
+            Nome: <input type="text" name="nome" required><br><br>
+            Login: <input type="text" name="login" required><br><br>
+            Senha: <input type="password" name="senha" required><br><br>
+            <input type="submit" value="Cadastrar">
+        </form>
+    </center>
+</body>
+</html>
